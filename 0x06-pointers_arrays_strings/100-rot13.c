14852f7a6aecabec7bfcda6e2caa72255fd1750b
Ndumbi <ndumbitshepe@gmail.com>
2023-07-27 10:02:06 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,43 +1,56 @@
 #include "main.h"
 
+#include <stdio.h>
+
+
 /**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: returns the encoded string
-*/
 
-char *rot13(char *str)
+ * rot13 - encoder rot13
+
+ * @s: pointer to string params
+
+ *
+
+ * Return: *s
+
+ */
+
+
+char *rot13(char *s)
+
 {
-	int index1, index2;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-											 'G', 'H', 'I', 'J', 'K', 'L',
-											 'M', 'N', 'O', 'P', 'Q', 'R',
-											 'S', 'T', 'U', 'V', 'W', 'X',
-											 'Y', 'Z', 'a', 'b', 'c', 'd',
-											 'e', 'f', 'g', 'h', 'i', 'j',
-											 'k', 'l', 'm', 'n', 'o', 'p',
-											 'q', 'r', 's', 't', 'u', 'v',
-											 'w', 'x', 'y', 'z'};
+        int i;
 
-	// Initialize the rot13key array
-	rot13key[0] = 'N';
-	for (index2 = 1; index2 < 52; index2++)
-	{
-		rot13key[index2] = alphabet[51 - index2];
-	}
+        int j;
 
-	while (str[++index1])
-	{
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (str[index1] == alphabet[index2])
-			{
-				str[index1] = rot13key[index2];
-				break;
-			}
-		}
-	}
-	return (str);
+        char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
+        for (i = 0; s[i] != '\0'; i++)
+
+        {
+
+                for (j = 0; j < 52; j++)
+
+                {
+
+                        if (s[i] == data1[j])
+
+                        {
+
+                                s[i] = datarot[j];
+
+                                break;
+
+                        }
+
+                }
+
+        }
+
+        return (s);
+
 }
-
